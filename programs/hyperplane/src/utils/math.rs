b0418fc1798fbcf6869a62c8b0f5cefb14db9364
Elliot Kennedy <me@elken.io>
2023-02-01 18:33:01 +0000

    Add log message on conversion failure

--- programs/hyperplane/src/utils/math.rs
+++ programs/hyperplane/src/utils/math.rs
@@ -1,9 +1,13 @@
 use crate::error::SwapError;
+use spl_token::solana_program::msg;
 
 pub fn to_u128(val: u64) -> u128 {
     val.into()
 }
 
 pub fn to_u64(val: u128) -> Result<u64, SwapError> {
-    val.try_into().map_err(|_| SwapError::ConversionFailure)
+    val.try_into().map_err(|_| {
+        msg!("Unable to convert u128 to u64: {}", val);
+        SwapError::ConversionFailure
+    })
 }
