19460fb59902175abe8fa38e09b38c6ec7e2019d
Elliot Kennedy <me@elken.io>
2023-02-07 17:40:12 +0000

    Fix clippy - remove unused features

--- programs/hyperplane/src/utils/seeds.rs
+++ programs/hyperplane/src/utils/seeds.rs
@@ -1,6 +1,6 @@
-pub const POOL_AUTHORITY: &'static [u8] = b"pauthority";
-pub const POOL_TOKEN_FEES_VAULT: &'static [u8] = b"lpfee";
-pub const POOL_TOKEN_MINT: &'static [u8] = b"lp";
-pub const SWAP_CURVE: &'static [u8] = b"curve";
-pub const TOKEN_A_VAULT: &'static [u8] = b"pvault_a";
-pub const TOKEN_B_VAULT: &'static [u8] = b"pvault_b";
+pub const POOL_AUTHORITY: &[u8] = b"pauthority";
+pub const POOL_TOKEN_FEES_VAULT: &[u8] = b"lpfee";
+pub const POOL_TOKEN_MINT: &[u8] = b"lp";
+pub const SWAP_CURVE: &[u8] = b"curve";
+pub const TOKEN_A_VAULT: &[u8] = b"pvault_a";
+pub const TOKEN_B_VAULT: &[u8] = b"pvault_b";
