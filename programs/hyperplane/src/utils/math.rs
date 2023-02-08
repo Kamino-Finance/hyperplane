7125aae3a61688b75cf3ec60504c651d8caf04ca
Elliot Kennedy <me@elken.io>
2023-02-08 01:14:13 +0000

    Convert math functions to return anchor results

--- programs/hyperplane/src/utils/math.rs
+++ programs/hyperplane/src/utils/math.rs
@@ -1,13 +1,14 @@
 use crate::error::SwapError;
-use anchor_lang::prelude::msg;
+use anchor_lang::error;
+use anchor_lang::prelude::*;
 
 pub fn to_u128(val: u64) -> u128 {
     val.into()
 }
 
-pub fn to_u64(val: u128) -> Result<u64, SwapError> {
+pub fn to_u64(val: u128) -> Result<u64> {
     val.try_into().map_err(|_| {
         msg!("Unable to convert u128 to u64: {}", val);
-        SwapError::ConversionFailure
+        error!(SwapError::ConversionFailure)
     })
 }
