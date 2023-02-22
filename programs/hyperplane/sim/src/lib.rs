5bbbedd61850d0b6573fa4ebeef5ab4a0e2f04d1
Elliot Kennedy <me@elken.io>
2023-02-22 04:14:50 +0000

    Migrate stable simulation to use BigInt internally

--- programs/hyperplane/sim/src/lib.rs
+++ programs/hyperplane/sim/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate core;
 
-use num_bigint::BigUint;
-use num_traits::ToPrimitive;
+use num_bigint::BigInt;
+use num_traits::{One, ToPrimitive, Zero};
 
 const DEFAULT_POOL_TOKENS: u128 = 0;
 const DEFAULT_TARGET_PRICE: u128 = 1000000000000000000;
@@ -10,23 +10,26 @@ pub const MODEL_FEE_DENOMINATOR: u128 = 1000;
 
 #[derive(Clone, Debug)]
 pub struct StableSwapModel {
-    pub amp_factor: u128,
-    pub balances: Vec<u128>,
-    pub n_coins: u8,
-    pub fee: u128,
-    pub target_prices: Vec<u128>,
-    pub pool_tokens: u128,
+    pub amp_factor: BigInt,
+    pub balances: Vec<BigInt>,
+    pub n_coins: BigInt,
+    pub fee: BigInt,
+    pub target_prices: Vec<BigInt>,
+    pub pool_tokens: BigInt,
 }
 
 impl StableSwapModel {
     pub fn new(amp_factor: u128, balances: Vec<u128>, n_coins: u8) -> StableSwapModel {
         Self {
-            amp_factor,
-            balances,
-            n_coins,
-            fee: 0,
-            target_prices: vec![DEFAULT_TARGET_PRICE, DEFAULT_TARGET_PRICE],
-            pool_tokens: DEFAULT_POOL_TOKENS,
+            amp_factor: BigInt::from(amp_factor),
+            balances: balances.iter().map(|x| BigInt::from(*x)).collect(),
+            n_coins: BigInt::from(n_coins),
+            fee: BigInt::zero(),
+            target_prices: vec![
+                BigInt::from(DEFAULT_TARGET_PRICE),
+                BigInt::from(DEFAULT_TARGET_PRICE),
+            ],
+            pool_tokens: BigInt::from(DEFAULT_POOL_TOKENS),
         }
     }
 
@@ -37,168 +40,186 @@ impl StableSwapModel {
         pool_token_amount: u128,
     ) -> StableSwapModel {
         Self {
-            amp_factor,
-            balances,
-            n_coins,
-            fee: 0,
-            target_prices: vec![DEFAULT_TARGET_PRICE, DEFAULT_TARGET_PRICE],
-            pool_tokens: pool_token_amount,
+            amp_factor: BigInt::from(amp_factor),
+            balances: balances.iter().map(|x| BigInt::from(*x)).collect(),
+            n_coins: BigInt::from(n_coins),
+            fee: BigInt::zero(),
+            target_prices: vec![
+                BigInt::from(DEFAULT_TARGET_PRICE),
+                BigInt::from(DEFAULT_TARGET_PRICE),
+            ],
+            pool_tokens: BigInt::from(pool_token_amount),
         }
     }
 
-    pub fn sim_xp(&self) -> Vec<u128> {
+    pub fn sim_xp(&self) -> Vec<BigInt> {
         self.balances
             .iter()
-            .map(|x| BigUint::from(*x))
-            .zip(self.target_prices.iter().map(|p| BigUint::from(*p)))
-            .map(|(x, p)| x * p / BigUint::from(10_u128.pow(18)))
-            .map(|x| x.to_u128().unwrap())
+            .zip(self.target_prices.iter())
+            .map(|(x, p)| x * p / BigInt::from(10).pow(18))
             .collect()
     }
 
     pub fn sim_d(&self) -> u128 {
-        let mut d_prev = 0;
+        let mut d_prev = BigInt::zero();
         let xp = self.sim_xp();
-        let s = xp.iter().fold(0, |acc, x| acc + x);
-        let mut d = s;
-        let ann = self.amp_factor * self.n_coins as u128;
+        let s = xp.iter().fold(BigInt::zero(), |acc, x| acc + x);
+        let mut d = s.clone();
+        let ann = &self.amp_factor * &self.n_coins;
 
-        while d.abs_diff(d_prev) > 1 {
-            let mut d_p = d;
+        while d.abs_diff(&d_prev) > BigInt::one() {
+            let mut d_p = d.clone();
             for x in xp.iter() {
-                d_p = (BigUint::from(d_p) * BigUint::from(d)
-                    / BigUint::from(self.n_coins as u128 * x))
-                .to_u128()
-                .unwrap();
+                d_p = d_p * &d / (&self.n_coins * x);
             }
-            d_prev = d;
+            d_prev = d.clone();
 
             // D = (AnnS + D_P * n) * D / ((Ann - 1) * D + (n + 1) * D_P)
 
-            let numerator = BigUint::from(ann * s + d_p * self.n_coins as u128) * BigUint::from(d);
-            let denominator = BigUint::from(ann - 1) * BigUint::from(d)
-                + BigUint::from(self.n_coins as u128 + 1) * BigUint::from(d_p);
+            let numerator = (&ann * &s + &d_p * &self.n_coins) * &d;
+            let denominator = (&ann - 1) * &d + (&self.n_coins + 1) * &d_p;
 
-            d = (numerator / denominator).to_u128().unwrap();
+            d = numerator / denominator;
         }
-        d
+        d.to_u128().unwrap()
     }
 
     pub fn sim_dy(&self, i: u128, j: u128, dx: u128) -> u128 {
-        return self.balances[j as usize] - self.sim_y(i, j, self.balances[i as usize] + dx);
+        self.balances[j as usize].to_u128().unwrap()
+            - self.sim_y(i, j, self.balances[i as usize].to_u128().unwrap() + dx)
     }
 
     pub fn sim_exchange(&mut self, i: u128, j: u128, dx: u128) -> u128 {
         let xp = self.sim_xp();
-        let x = xp[i as usize] + dx;
-        let y = self.sim_y(i, j, x);
-        let dy = xp[j as usize] - y;
-        let fee = dy * self.fee / 10_u128.pow(10);
+        let x = &xp[i as usize] + BigInt::from(dx);
+        let y = self.sim_y(i, j, x.to_u128().unwrap());
+        let dy = &xp[j as usize] - y;
+        let fee = &dy * &self.fee / BigInt::from(10).pow(10);
 
-        self.balances[i as usize] = x * 10_u128.pow(18) / self.target_prices[i as usize];
-        self.balances[j as usize] = (y + fee) * 10_u128.pow(18) / self.target_prices[i as usize];
+        self.balances[i as usize] = &x * BigInt::from(10).pow(18) / &self.target_prices[i as usize];
+        self.balances[j as usize] =
+            (y + &fee) * BigInt::from(10).pow(18) / &self.target_prices[i as usize];
 
-        dy - fee
+        (&dy - &fee).to_u128().unwrap()
     }
 
     pub fn sim_y(&self, i: u128, j: u128, x: u128) -> u128 {
-        let d = self.sim_d();
+        let d = BigInt::from(self.sim_d());
         let mut xx = self.sim_xp();
-        xx[i as usize] = x; // x is quantity of underlying asset brought to 1e18 precision
+        xx[i as usize] = BigInt::from(x); // x is quantity of underlying asset brought to 1e18 precision
 
         let mut new_xx = vec![];
-        for k in 0..self.n_coins {
+        for (k, amt) in xx.iter().enumerate().take(self.n_coins.to_usize().unwrap()) {
             if k as u128 != j {
-                new_xx.push(xx[k as usize]);
+                new_xx.push(amt.clone());
             }
         }
         // remove x[j] from xx
         xx = new_xx;
-        let ann = self.amp_factor * self.n_coins as u128;
-        let mut c = d;
+        let ann = &self.amp_factor * &self.n_coins;
+        let mut c = d.clone();
 
         for y in xx.iter() {
-            c = c * d / (y * self.n_coins as u128);
+            c = &c * &d / (y * &self.n_coins);
         }
-        c = c * d / (self.n_coins as u128 * ann);
-        let b = xx.iter().fold(0, |acc, x| acc + x) + d / ann;
+        c = &c * &d / (&self.n_coins * &ann);
+        let b = xx.iter().fold(BigInt::zero(), |acc, x| acc + x) + &d / &ann;
 
-        let mut y_prev = 0;
-        let mut y = d;
-        while y.abs_diff(y_prev) > 1 {
-            y_prev = y;
-            y = (y.pow(2) + c) / (2 * y + b - d);
+        let mut y_prev = BigInt::zero();
+        let mut y = d.clone();
+        while y.abs_diff(&y_prev) > BigInt::one() {
+            y_prev = y.clone();
+            // y = y**2 + c / 2y + b - D
+            y = (y.pow(2) + &c) / (2 * &y + &b - &d);
         }
-        y
+        y.to_u128().unwrap()
     }
 
     pub fn sim_y_d(&mut self, i: u128, d: u128) -> u128 {
+        let d = BigInt::from(d);
         let mut xx = self.sim_xp();
         let mut new_xx = vec![];
-        for k in 0..self.n_coins {
+        for (k, amt) in xx.iter().enumerate().take(self.n_coins.to_usize().unwrap()) {
             if k as u128 != i {
-                new_xx.push(xx[k as usize]);
+                new_xx.push(amt.clone());
             }
         }
         // remove x[i] from xx
         xx = new_xx;
-        let s = xx.iter().fold(0, |acc, x| acc + x);
-        let ann = self.amp_factor * self.n_coins as u128;
-        let mut c = d;
+        let s = xx.iter().fold(BigInt::zero(), |acc, x| acc + x);
+        let ann = &self.amp_factor * &self.n_coins;
+        let mut c = d.clone();
         for y in xx.iter() {
-            c = c * d / (y * self.n_coins as u128);
+            c = &c * &d / (y * &self.n_coins);
         }
-        c = c * d / (ann * self.n_coins as u128);
-        let b = s + d / ann - d;
-        let mut y_prev = 0;
-        let mut y = d;
-        while y.abs_diff(y_prev) > 1 {
-            y_prev = y;
-            y = (y.pow(2) + c) / (2 * y + b - d);
+        c = &c * &d / (&ann * &self.n_coins);
+        let b = &s + &d / &ann - &d;
+        let mut y_prev = BigInt::zero();
+        let mut y = d.clone();
+        while y.abs_diff(&y_prev) > BigInt::one() {
+            y_prev = y.clone();
+            // y = y**2 + c / 2y + b - D
+            y = (y.pow(2) + &c) / (2 * &y + &b - &d);
         }
-        y
+        y.to_u128().unwrap()
     }
 
     pub fn sim_remove_liquidity_imbalance(&mut self, amounts: Vec<u128>) -> u128 {
-        let fee = self.fee * self.n_coins as u128 / (4 * (self.n_coins as u128 - 1));
+        let fee = &self.fee * &self.n_coins / (4 * (&self.n_coins - 1));
         let old_balances = self.balances.clone();
         let mut new_balances = self.balances.clone();
         let d0 = self.sim_d();
-        for i in 0..self.n_coins {
-            new_balances[i as usize] -= amounts[i as usize];
+        for i in 0..self.n_coins.to_usize().unwrap() {
+            new_balances[i] -= amounts[i];
         }
         self.balances = new_balances.clone();
         let d1 = self.sim_d();
         self.balances = old_balances.clone();
-        let mut fees: Vec<u128> = new_balances.iter().map(|_| 0).collect();
-        for i in 0..self.n_coins {
-            let ideal_balance = d1 * old_balances[i as usize] / d0;
-            let difference = ideal_balance.abs_diff(new_balances[i as usize]);
-            fees[i as usize] = fee * difference / 10_u128.pow(10);
-            new_balances[i as usize] -= fees[i as usize];
+        let mut fees: Vec<BigInt> = new_balances.iter().map(|_| BigInt::zero()).collect();
+        for i in 0..self.n_coins.to_usize().unwrap() {
+            let ideal_balance = d1 * &old_balances[i] / d0;
+            let difference = ideal_balance.abs_diff(&new_balances[i]);
+            fees[i] = &fee * difference / BigInt::from(10).pow(10);
+            new_balances[i] -= fees[i].clone();
         }
         self.balances = new_balances.clone();
         let d2 = self.sim_d();
-        self.balances = old_balances.clone();
+        self.balances = old_balances;
 
-        let token_amount = (d0 - d2) * self.pool_tokens / d0;
+        let token_amount = (d0 - d2) * &self.pool_tokens / d0;
 
-        token_amount
+        token_amount.to_u128().unwrap()
     }
 
     pub fn sim_calc_withdraw_one_coin(&mut self, token_amount: u128, i: u128) -> u128 {
         let xp = self.sim_xp();
-        let sum_xp = xp.iter().fold(0, |acc, x| acc + x);
-        let fee = if self.fee > 0 {
-            self.fee - self.fee * xp[i as usize] / sum_xp + 5 * 10_u128.pow(5)
+        let sum_xp = xp.iter().fold(BigInt::zero(), |acc, x| acc + x);
+        let fee = if self.fee > BigInt::zero() {
+            &self.fee - &self.fee * &xp[i as usize] / &sum_xp + 5 * BigInt::from(10).pow(5)
         } else {
-            0
+            BigInt::zero()
         };
 
         let d0 = self.sim_d();
-        let d1 = d0 - token_amount * d0 / self.pool_tokens;
-        let dy = xp[i as usize] - self.sim_y_d(i, d1);
+        let d1 = d0 - token_amount * d0 / &self.pool_tokens;
+        let dy = &xp[i as usize] - self.sim_y_d(i, d1.to_u128().unwrap());
 
-        dy - dy * fee / 10_u128.pow(10)
+        (&dy - &dy * &fee / BigInt::from(10).pow(10))
+            .to_u128()
+            .unwrap()
+    }
+}
+
+trait AbsDiff {
+    fn abs_diff(&self, other: &Self) -> Self;
+}
+
+impl AbsDiff for BigInt {
+    fn abs_diff(&self, other: &BigInt) -> BigInt {
+        if self > other {
+            self - other
+        } else {
+            other - self
+        }
     }
 }
